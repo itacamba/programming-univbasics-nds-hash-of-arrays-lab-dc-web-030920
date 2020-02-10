dc24feaa1290eb2640e9c483269fee740b1be658
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-10 05:09:34 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,6 +9,5 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   #BIG Note we can not push values into hashes with .push or <<
-  show << name
   p show
 end
