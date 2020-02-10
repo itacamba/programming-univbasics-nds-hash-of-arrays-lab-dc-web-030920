6e29eda154f95563244ec7c750d0e93ecf9198ed
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-10 05:07:40 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -11,17 +11,3 @@ def add_character(show, name)
   #BIG Note we can not push values into hashes with .push or <<
   p show
 end
-
-
-my_hoa = {
-  lunes: ["Salmon","Chicken"],
-  martes: ["Beef", "Pho"],
-  miercoles: ["Rice", "Veggies"]
-}
-row_index = 0
-show = {}
-while row_index < my_hoa.length do
-  show.push(my_hoa[row_index])
-  row_index += 1
-end
-  p show
