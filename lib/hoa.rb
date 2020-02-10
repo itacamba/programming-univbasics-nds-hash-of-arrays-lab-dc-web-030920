611a52cfdb18b9be7fd90953298c63d0f5e0c14e
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-10 05:07:34 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,13 +8,7 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
-  row_index = 0
-  show = []
-  while row_index < BASE_HOA.length do
-    BASE_HOA[row_index].push(name)
-    row_index += 1
-  end
-  show.push(BASE_HOA[row_index])
+  #BIG Note we can not push values into hashes with .push or <<
   p show
 end
 
@@ -27,7 +21,7 @@ my_hoa = {
 row_index = 0
 show = {}
 while row_index < my_hoa.length do
-  show << my_hoa[row_index]
+  show.push(my_hoa[row_index])
   row_index += 1
 end
   p show
